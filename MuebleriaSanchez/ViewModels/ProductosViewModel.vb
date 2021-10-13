9c95e3d4a72841e9cc7698bb48d652424f8abce0
evecg29 <evelynmariela08@hotmail.com>
2021-10-13 18:57:08 -0500

    creando views de productos

---
+++ MuebleriaSanchez/ViewModels/ProductosViewModel.vb
@@ -0,0 +1,46 @@
+﻿Public Class ProductosViewModel
+    Inherits BaseViewModel
+
+    Private _context As MainViewModel
+
+    Public Property OpenOptionCommand As ICommand
+        Get
+            Return _OpenOptionCommand
+        End Get
+        Set(value As ICommand)
+            _OpenOptionCommand = value
+            OnPropertyChanged(NameOf(OpenOptionCommand))
+        End Set
+    End Property
+
+    Private Property _OpenOptionCommand As ICommand
+
+    Public Sub New(context As MainViewModel)
+        _context = context
+        OpenOptionCommand = New DelegateCommand(AddressOf OpenOption, AddressOf canOpenOption)
+    End Sub
+
+    Public Sub OpenOption(optionId As Integer)
+        Select Case optionId
+            Case 1
+                Debug.Print("SIrve")
+
+            Case 2
+                '
+            Case 3
+
+            Case 4
+
+            Case 5
+
+            Case 6
+
+        End Select
+    End Sub
+
+    Public Function canOpenOption() As Boolean
+        Return True
+    End Function
+
+
+End Class
