a8880c377f342c4aca1824427e61d04e4bec9cff
evecg29 <evelynmariela08@hotmail.com>
2021-10-17 11:47:55 -0500

    tratando de conectar la base de datos

---
+++ MuebleriaSanchez/ViewModels/ConexionBDViewModel.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Data
+Imports System.Data.OleDb
+
+Module ConexionBDViewModel
+    Public conexion As New OleDbConnection
+    Public estado As String
+    Public comando As New OleDbCommand
+
+
+    Sub enlace()
+        Try
+            conexion.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Resources/BASEDATOS.accdb"
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Module
